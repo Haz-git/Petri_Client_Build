f785028896e0d87bfeeb9fba94e630d90ea754df
Haz-git <zhouharry1@gmail.com>
2020-12-02 05:56:20 -0800

    Adding conditional for reroute on JWT

--- frontend/src/components/landingPage/MainLandingPage.js
+++ frontend/src/components/landingPage/MainLandingPage.js
@@ -1,5 +1,8 @@
 //Dependencies:
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { getJWT } from '../../utils/jwthelper';
+import history from '../../historyObject';
 
 //Components
 import MainBackdrop from './MainBackdrop';
@@ -7,6 +10,16 @@ import MainBodyblock from './MainBodyblock';
 import Footer from './Footer';
 
 const MainLandingPage = () => {
+
+    useEffect(() => {
+        const jwtCheck = getJWT();
+
+        if (jwtCheck !== undefined && jwtCheck !== null) {
+            history.push('/dashboard');
+        }
+    }, [])
+
+
     return (
         <>
             <MainBackdrop />
