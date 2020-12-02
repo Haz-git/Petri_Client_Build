7caa346ff586dd4ac9394fde631040ab8c1ec06c
Haz-git <zhouharry1@gmail.com>
2020-12-02 14:37:42 -0800

    Adding settings for lacZ

--- frontend/src/components/Dashboard/sciTools/LacZ.js
+++ frontend/src/components/Dashboard/sciTools/LacZ.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import LacZCards from './LacZCards';
 import styled from 'styled-components';
@@ -8,6 +8,16 @@ import { Link } from 'react-router-dom';
 import { MainHeader, SecondaryHeader } from '../../signupPage/SignUpForm';
 import { Button } from 'react-bootstrap';
 import { ArrowLeftSquare } from '@styled-icons/bootstrap/ArrowLeftSquare';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import { Cogs } from '@styled-icons/fa-solid/Cogs';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+
+
+const StyledMainHeader = styled(MainHeader)`
+    margin: 0;
+`
 
 const LacZMainContainer = styled.div`
     padding: 20px 20px;
@@ -28,19 +38,33 @@ const ArrowIcon = styled(ArrowLeftSquare)`
     margin-right: 7px;
 `
 
+const StyledCogs = styled(Cogs)`
+    height: 21px;
+    width: 21px;
+    margin-right: 7px;
+`
+const AccordionContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
 //Render:
 
 const LacZ = ({ laczAssayProtocols, match:{params:{id}} }) => {
 
     const ownProtocol = laczAssayProtocols.find(x => x.protocolId === id);
 
+    const [ volume, setVolume ] = useState(0.05);
+    const [ dilution, setDilution ] = useState(5);
+    const [ minutes, setMinutes ] = useState(1.3333);
+
     const renderLacZCards = () => {
         if (ownProtocol.collectionStrains !== null && ownProtocol.collectionStrains !== undefined) {
             return ownProtocol.collectionStrains.map(strain => (
                 <LacZCards
-                    minute={1.333333}
-                    dilutionFactor={5}
-                    volume={0.05}
+                    minute={minutes}
+                    dilutionFactor={dilution}
+                    volume={volume}
                     ownProtocolId={ownProtocol.protocolId}
                     ownStrainId={strain.strainId}
                     ownStrainName={strain.strainName}
@@ -61,11 +85,26 @@ const LacZ = ({ laczAssayProtocols, match:{params:{id}} }) => {
         }
     }
 
+    const handleOnVolumeChange = e => {
+        setVolume(e.target.value);
+    }
+
+    const handleOnDilutionChange = e => {
+        setDilution(e.target.value);
+    }
+
+    const handleOnMinutesChange = e => {
+        setMinutes(e.target.value);
+    }
+
+    // const handleSettingsSubmit = () => {
+
+    // }
+
 
     return (
         <LacZMainContainer>
             <HeaderContainer>
-                <MainHeader>LacZ Data Collection</MainHeader>
                 <ReturnButtonContainer>
                     <Link to='/scitools/lazylacz'>
                         <Button variant='dark'>
@@ -74,6 +113,62 @@ const LacZ = ({ laczAssayProtocols, match:{params:{id}} }) => {
                         </Button>
                     </Link>
                 </ReturnButtonContainer>
+                <StyledMainHeader>LacZ Data Collection</StyledMainHeader>
+                <AccordionContainer>
+                    <Accordion>
+                            <Card>
+                                <Card.Header>
+                                    <Accordion.Toggle as={Button} variant='dark' eventKey='0'>
+                                        <StyledCogs />
+                                        Configure
+                                    </Accordion.Toggle>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey='0'>
+                                    <Card.Body>
+                                        <InputGroup className="mb-3">
+                                            <InputGroup.Prepend>
+                                                <InputGroup.Text id="basic-addon1">Volume</InputGroup.Text>
+                                            </InputGroup.Prepend>
+                                            <FormControl
+                                            placeholder="0.05"
+                                            aria-label="Volume"
+                                            aria-describedby="basic-addon1"
+                                            type='number'
+                                            onChange={handleOnVolumeChange}
+                                            />
+                                        </InputGroup>
+                                        <InputGroup className="mb-3">
+                                            <InputGroup.Prepend>
+                                                <InputGroup.Text id="basic-addon1">Dilution Factor</InputGroup.Text>
+                                            </InputGroup.Prepend>
+                                            <FormControl
+                                            placeholder="5"
+                                            aria-label="Dilution Factor"
+                                            aria-describedby="basic-addon1"
+                                            type='number'
+                                            onChange={handleOnDilutionChange}
+                                            />
+                                        </InputGroup>
+                                        <InputGroup className="mb-3">
+                                            <InputGroup.Prepend>
+                                                <InputGroup.Text id="basic-addon1">Minutes Taken</InputGroup.Text>
+                                            </InputGroup.Prepend>
+                                            <FormControl
+                                            placeholder="1.3333"
+                                            aria-label="Minutes Taken"
+                                            aria-describedby="basic-addon1"
+                                            type='number'
+                                            onChange={handleOnMinutesChange}
+                                            />
+                                        </InputGroup>
+                                        {/* <Button variant='primary' onClick={handleSettingsSubmit}>
+                                            Submit
+                                        </Button> */}
+                                    </Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        </Accordion>
+                    </AccordionContainer>
             </HeaderContainer>
             <div>
                 {renderLacZCards()}
