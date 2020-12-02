bd3afb84715c3f317372769cb52bdb21c6aeb3ac
Haz-git <zhouharry1@gmail.com>
2020-12-02 08:07:48 -0800

    Forgot to call function

--- frontend/src/components/Dashboard/messenger/ChatCard.js
+++ frontend/src/components/Dashboard/messenger/ChatCard.js
@@ -16,7 +16,7 @@ const ChatCard = (props) => {
     return (
         <div>
             <div>
-                <h2>{props.sender.userName}</h2>
+                <h2>{renderNames()}</h2>
                 <h3>{props.sender.createdAt}</h3>
             </div>
             <div>{props.message}</div>
