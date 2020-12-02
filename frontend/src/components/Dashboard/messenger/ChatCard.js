0937e94c3f624a9951d31361698c0cb919558d0b
Haz-git <zhouharry1@gmail.com>
2020-12-02 08:33:15 -0800

    Removed ChatCard conditional render, will continue later. Also added favicon

--- frontend/src/components/Dashboard/messenger/ChatCard.js
+++ frontend/src/components/Dashboard/messenger/ChatCard.js
@@ -2,19 +2,10 @@ import React from 'react';
 
 const ChatCard = (props) => {
 
-    const renderNames = () => {
-        if(props.sender.userName !== null && props.sender.userName !== undefined) {
-            return <h2>{props.sender.userName}</h2>
-        } else {
-            return <h2>Deleted User</h2>
-        }
-    }
-
-
     return (
         <div>
             <div>
-                <h2>{renderNames()}</h2>
+                <h2>{props.sender.userName}</h2>
                 <h3>{props.sender.createdAt}</h3>
             </div>
             <div>{props.message}</div>
