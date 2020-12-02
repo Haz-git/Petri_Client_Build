2e113005717676636d26404bdb8f979f8facd2a4
Haz-git <zhouharry1@gmail.com>
2020-12-02 05:56:35 -0800

    Removing CLG

--- frontend/src/redux/userTaskLog/userTaskLogActions.js
+++ frontend/src/redux/userTaskLog/userTaskLogActions.js
@@ -6,8 +6,6 @@ export function getTasks() {
         const { auth: { userLogIn: { data: { _id } }} } = getState();
         const response = await api.post('/users/getTasks', { _id });
 
-        console.log(response);
-
         dispatch({
             type: USER_GET_TASKS,
             payload: response.data.existingUserTaskList,
