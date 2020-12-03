731442adffd918db3da5250ed7c46c8b6a83b747
Haz-git <zhouharry1@gmail.com>
2020-12-03 15:01:29 -0800

    Rendered Charts and provided routing button

--- frontend/src/components/Dashboard/sciTools/LacZCompareCharts.js
+++ frontend/src/components/Dashboard/sciTools/LacZCompareCharts.js
@@ -1,16 +1,114 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Label, ResponsiveContainer } from 'recharts';
+import { Link } from 'react-router-dom';
 
+//Styles:
+import { Button } from 'react-bootstrap';
+import { ArrowLeftSquare } from '@styled-icons/bootstrap/ArrowLeftSquare';
+
+const MainContainer = styled.div`
+    margin: 35px 35px;
+    background-color: white;
+    border: 1px solid white;
+    border-radius: 5px;
+    padding: 10px 10px;
+    text-align: center;
+    box-shadow:
+        0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+        0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+        0 12.5px 5px rgba(0, 0, 0, 0.06),
+        0 22.3px 10px rgba(0, 0, 0, 0.072),
+        0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+        0 60px 80px rgba(0, 0, 0, 0.12);
+    ;
+`
+
+const MainHeader = styled.h1`
+    margin: 0;
+    font-weight: 900;
+    font-size: 50px;
+    padding-top: 10px;
+    padding-right: 10px;
+    padding-left: 10px;
+    margin-bottom: 20px;
+    color: #242746;
+    font-family: 'Montserrat', sans-serif;
+`
+
+const ChartContainer = styled.div`
+    width: 100%;
+    height: 600px;
+    margin-top: 20px;
+    margin-bottom: 40px;
+`
+
+const ArrowIcon = styled(ArrowLeftSquare)`
+    height: 21px;
+    width: 21px;
+    margin-right: 7px;
+    vertical-align: sub;
+`
+
+
+//Render:
 const LacZCompareCharts = ({ laczAssayProtocols, match:{params:{id}} }) => {
 
     const ownProtocol = laczAssayProtocols.find(x => x.protocolId === id);
 
-    console.log(ownProtocol);
+    let comparisonData;
+    let comparisonArray = [];
+
+
+    if (ownProtocol.collectionStrains !== null && ownProtocol.collectionStrains !== undefined) {
+        comparisonData = {...ownProtocol};
+    }
+
+    console.log(comparisonData);
+
+    if (comparisonData !== null && comparisonData !== undefined) {
+        for(let k = 0; k < comparisonData.collectionStrains.length; k++) {
+            let comparisonObject = {
+                name: comparisonData.collectionStrains[k].strainName,
+                bgalAverage: comparisonData.collectionStrains[k].bgalData.bgalSlope,
+            }
+
+            comparisonArray.push(comparisonObject);
+        }
+    }
+
+    console.log(comparisonArray);
+
 
 
     return (
         <>
-            This page is currently under construction...
+            <MainContainer>
+                <MainHeader>Your Activities</MainHeader>
+                <ChartContainer>
+                    <ResponsiveContainer>
+                        <BarChart width={730} height={250} data={comparisonArray} margin={{ top: 0, right: 20, left: 30, bottom: 40 }}>
+                            <CartesianGrid strokeDasharray="5 5" />
+                            <XAxis dataKey="name">
+                                <Label value='Strains' position='bottom' style={{ textAnchor: 'middle' }} />
+                            </XAxis>
+                            <YAxis>
+                                <Label value='B-Galactosidase Activity' position='left' angle={-90} style={{ textAnchor: 'middle' }} />
+                            </YAxis>
+                            <Tooltip />
+                            <Legend verticalAlign="top" height={40}/>
+                            <Bar dataKey="bgalAverage" fill="#242746" barSize={50} />
+                        </BarChart>
+                    </ResponsiveContainer>
+                </ChartContainer>
+                    <Link to={`/scitools/lazylacz/lacz/${id}`}>
+                        <Button variant='dark'>
+                            <ArrowIcon />
+                            My LacZ Data
+                        </Button>
+                    </Link>
+            </MainContainer>
         </>
     )
 }
