32f92e8e942d5295c1480dac2ce51ed105e7b61d
Haz-git <zhouharry1@gmail.com>
2020-12-07 05:35:08 -0800

    Adding DnD + React Avatar Editor

--- frontend/src/components/Dashboard/settings/EditProfilePicture.js
+++ frontend/src/components/Dashboard/settings/EditProfilePicture.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AvatarEditor from 'react-avatar-editor';
+import DropZone from 'react-dropzone';
 
 const EditProfilePicture = () => {
+
+    const [ image, setImage ] = useState('');
+    const [ width, setWidth ] = useState(400);
+    
+    const handleDrop = dropped => {
+        console.log(dropped);
+        setImage(dropped[0]);
+    }
+
     return (
         <div>
-            This is the page for editing a new avatar picture;
+            <h1>This is experimental</h1>
+            <div>
+                <DropZone
+                    onDrop={handleDrop}
+                    noClick
+                    noKeyboard
+                    style={{ width: '250px', height: '250px'}}
+                >
+                    {({ getRootProps, getInputProps }) => (
+                        <div {...getRootProps()}>
+                            <AvatarEditor
+                                width={400}
+                                height={400} 
+                                image={image}
+                            />
+                            <input {...getInputProps()} />
+                        </div>
+                    )}
+                </DropZone>
+            </div>
+            <div>
+                <h2>This is the options container</h2>
+                <div>
+
+                </div>
+            </div>
         </div>
     )
 }
