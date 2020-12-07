93e6f210d9a54a0102c809c513652e5c4b14a38f
Haz-git <zhouharry1@gmail.com>
2020-12-07 10:56:44 -0800

    Dispatched response to state

--- frontend/src/redux/userSettings/UserSettingActions.js
+++ frontend/src/redux/userSettings/UserSettingActions.js
@@ -5,15 +5,11 @@ export function userAddNewProfilePicture(imgURL, imgConstraints) {
     return async (dispatch, getState) => {
         const { auth: { userLogIn: { data: { _id } }} } = getState();
 
-        console.log('ActionController');
-
         const response = await api.post('/users/settings/setNewProPic', { _id, imgURL, imgConstraints });
 
-        console.log(response);
-
-        // dispatch({
-        //     type: USER_ADD_NEW_PROFILE_PICTURE,
-        //     payload: response.data.profileImg,
-        // })
+        dispatch({
+            type: USER_ADD_NEW_PROFILE_PICTURE,
+            payload: response.data.responseUpdatedProfilePic,
+        })
     }
 }
