799968155e021145c0c3161a6f4fcddc00a3e3e1
Haz-git <zhouharry1@gmail.com>
2020-12-07 15:30:46 -0800

    Added action, reducer, and type for profilepic retrieval

--- frontend/src/redux/userSettings/UserSettingActions.js
+++ frontend/src/redux/userSettings/UserSettingActions.js
@@ -1,5 +1,21 @@
 import api from '../../api';
-import { USER_ADD_NEW_PROFILE_PICTURE } from './UserSettingTypes';
+import { 
+    USER_ADD_NEW_PROFILE_PICTURE,
+    USER_GET_PROFILE_PICTURE,
+} from './UserSettingTypes';
+
+export function userGetProfilePicture() {
+    return async (dispatch, getState) => {
+        const { auth: { userLogIn: { data: { _id } }} } = getState();
+
+        const response = await api.post('/users/settings/getProPic', { _id });
+        
+        dispatch({
+            type: USER_GET_PROFILE_PICTURE,
+            payload: response.data.userExistingImg.profileImg
+        })
+    }
+}
 
 export function userAddNewProfilePicture(imgURL, imgConstraints) {
     return async (dispatch, getState) => {
--- frontend/src/redux/userSettings/UserSettingReducer.js
+++ frontend/src/redux/userSettings/UserSettingReducer.js
@@ -1,10 +1,15 @@
-import { USER_ADD_NEW_PROFILE_PICTURE } from './UserSettingTypes';
+import {
+    USER_ADD_NEW_PROFILE_PICTURE,
+    USER_GET_PROFILE_PICTURE,
+} from './UserSettingTypes';
 
 const initialState = {};
 
 export default (state = initialState, action) => {
     switch(action.type) {
         case USER_ADD_NEW_PROFILE_PICTURE:
+            return {...state, userSettings: action.payload};
+        case USER_GET_PROFILE_PICTURE:
             return {...state, userSettings: action.payload};
         default:
             return state;
