abe4c4c10c091b52118edab35fddcb526a638eac
Haz-git <zhouharry1@gmail.com>
2020-12-07 15:28:33 -0800

    Styled and rendered profile picture and snippet information

--- frontend/src/components/Dashboard/homePage/MainDashboard.js
+++ frontend/src/components/Dashboard/homePage/MainDashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { getJWT } from '../../../utils/jwthelper';
 import { connect } from 'react-redux';
 import defaultAvatar from '../../../Img/default_avatar.png';
+import { userGetProfilePicture } from '../../../redux/userSettings/UserSettingActions';
+import Fade from 'react-reveal/Fade';
 
 
 import styled from 'styled-components';
@@ -9,7 +11,6 @@ import styled from 'styled-components';
 //Main Dashboard Components:
 import MDTodo from './MDTodo';
 import MDSciNews from './MDSciNews';
-import { isNull } from 'lodash';
 
 //Styles:
 const MainDashboardContainer = styled.div`
@@ -17,18 +18,51 @@ const MainDashboardContainer = styled.div`
     background-color: #F6F9FC;
 `
 const MainDashboardHeaderContainer = styled.div`
-    padding-top: 20px;
+    padding-left: 40px;
+    display: flex;
     text-align: center;
-    background-color: #F6F9FC;
-`
-const MainDashboardHeader = styled.h1`
-    font-family: 'Nunito', sans-serif;
+    background-color: #1c1e37;
+    height: 110px;
+    border-left: 1px solid #F6F9FC;
+    justify-content: flex-start;
+    align-items: center;
     margin: 0;
+    padding-top: 0;
+    padding-bottom: 0;
+`
+const MainDashboardUserNameHeader = styled.h1`
+    font-family: 'Montserrat', sans-serif;
+    margin: 0;
+    font-size: 28px;
+    font-weight: 500;
+    color: #F6F9FC;
+    display: flex;
+    justify-content: flex-start;
+    padding-left: 15px;
+    padding-bottom: 5px;
+`
+
+const MainDashboardHeader = styled.h2`
+    font-family: 'Montserrat', sans-serif;
+    margin: 0;
+    font-size: 16px;
+    font-weight: 100;
+    color: #F6F9FC;
+    display: flex;
+    justify-content: flex-start;
+    padding-left: 15px;
 `
 const MainDashboardHeader2 = styled.h2`
-    font-family: 'Nunito', sans-serif;
+    font-family: 'Montserrat', sans-serif;
     padding-top: 10px;
     margin: 0;
+    font-size: 12px;
+    font-weight: 100;
+    color: #F6F9FC;
+    display: flex;
+    justify-content: flex-start;
+    padding-left: 15px;
+    padding-bottom: 5px;
 `
 
 const MainDashboardUpperGrid = styled.div`
@@ -43,10 +77,35 @@ const UpperGridContainer = styled.div`
     padding: 10px 10px;
 `
 
+const StyledCustomAvatar = styled.img`
+    height: 90px;
+    width: 90px;
+    border-radius: 50%;
+    vertical-align: middle;
+    border: 2px solid white;
+`
+
 const StyledDefaultAvatar = styled.img`
-    height: 125px;
-    width: 125px;
+    height: 80px;
+    width: 80px;
     background-color: white;
+    border-radius: 50%;
+    vertical-align: middle;
+    border: 2px solid white;
+    
+`
+const HelperSpan = styled.span`
+    display: inline-block;
+    vertical-align: middle;
+    height: 100%;
+`
+
+const DetailsContainer = styled.div`
+    margin: 0;
+    border-right: 1px solid white;
+    padding-right: 40px;
+    height: 110px;
+    padding-top: 10px;
 `
 
 class MainDashboard extends Component {
@@ -58,6 +117,7 @@ class MainDashboard extends Component {
     componentDidMount() {
 
         const userDetails = getJWT();
+        this.props.userGetProfilePicture();
 
         const { firstName, lastName, userName, email } = userDetails.data;
 
@@ -72,18 +132,16 @@ class MainDashboard extends Component {
     renderDashBoardProfilePicture = () => {
         if (this.props.myProfileImg.userSettings !== undefined && this.props.myProfileImg !== null) {
 
-            const { url, constraints } = this.props.myProfileImg.userSettings.profileImg;
-
-        
+            const { url, constraints } = this.props.myProfileImg.userSettings;
             return (
                 <>
-                    <img src={url} />
+                    <HelperSpan /><StyledCustomAvatar src={url} />
                 </>
             )
         } else {
             return (
                 <>
-                    <StyledDefaultAvatar src={defaultAvatar} />
+                    <HelperSpan /><StyledDefaultAvatar src={defaultAvatar} />
                 </>
             )
         }
@@ -92,16 +150,21 @@ class MainDashboard extends Component {
 
     render() {
 
-        const { firstName, email } = this.state;
+        const { firstName, lastName, userName, email } = this.state;
 
         return (
             <MainDashboardContainer>
                 <MainDashboardHeaderContainer>
-                    <MainDashboardHeader>Welcome back {firstName}! </MainDashboardHeader>
-                    <MainDashboardHeader2>You are currently signed in under {email}</MainDashboardHeader2>
-                    <div>
-                        {this.renderDashBoardProfilePicture()}
-                    </div>
+                    <Fade>
+                        <div>
+                            {this.renderDashBoardProfilePicture()}
+                        </div>
+                        <DetailsContainer>
+                            <MainDashboardUserNameHeader>{userName} </MainDashboardUserNameHeader>
+                            <MainDashboardHeader>{firstName}, {lastName} </MainDashboardHeader>
+                            <MainDashboardHeader2>{email}</MainDashboardHeader2>
+                        </DetailsContainer>
+                    </Fade>
                 </MainDashboardHeaderContainer>
                 <MainDashboardUpperGrid>
                     <UpperGridContainer>
@@ -122,4 +185,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(MainDashboard);
+export default connect(mapStateToProps, { userGetProfilePicture })(MainDashboard);
