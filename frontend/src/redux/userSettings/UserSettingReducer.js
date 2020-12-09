5c16e87e230e1bb6011e81cb0c42b30815e35ee5
Haz-git <zhouharry1@gmail.com>
2020-12-09 06:09:43 -0800

    Added action types to userSetting reducer

--- frontend/src/redux/userSettings/UserSettingReducer.js
+++ frontend/src/redux/userSettings/UserSettingReducer.js
@@ -1,6 +1,10 @@
 import {
     USER_ADD_NEW_PROFILE_PICTURE,
     USER_GET_PROFILE_PICTURE,
+    USER_CHANGE_EMAIL_ADDRESS,
+    USER_CHANGE_FIRSTNAME,
+    USER_CHANGE_LASTNAME,
+    USER_CHANGE_USERNAME,
 } from './UserSettingTypes';
 
 const initialState = {};
@@ -10,6 +14,14 @@ export default (state = initialState, action) => {
         case USER_ADD_NEW_PROFILE_PICTURE:
             return {...state, userSettings: action.payload};
         case USER_GET_PROFILE_PICTURE:
+            return {...state, userSettings: action.payload};
+        case USER_CHANGE_EMAIL_ADDRESS:
+            return {...state, userSettings: action.payload};
+        case USER_CHANGE_FIRSTNAME:
+            return {...state, userSettings: action.payload};
+        case USER_CHANGE_LASTNAME:
+            return {...state, userSettings: action.payload};
+        case USER_CHANGE_FIRSTNAME:
             return {...state, userSettings: action.payload};
         default:
             return state;
