7be987eb7ca66b732a28a88667aeff84da63d0f3
Haz-git <zhouharry1@gmail.com>
2020-12-10 12:11:28 -0800

    .

--- frontend/src/components/Dashboard/homePage/MainDashboard.js
+++ frontend/src/components/Dashboard/homePage/MainDashboard.js
@@ -164,13 +164,20 @@ class MainDashboard extends Component {
 
     renderDashBoardProfilePicture = () => {
         if (this.props.userDetails !== undefined && this.props.userDetails !== null) {
-
-            const { url, constraints } = this.props.userDetails.profileImg;
-            return (
-                <>
-                    <HelperSpan /><StyledCustomAvatar src={url} />
-                </>
-            )
+            if (this.props.userDetails.profileImg !== undefined && this.props.userDetails.profileImg !== null) {
+                const { url, constraints } = this.props.userDetails.profileImg;
+                return (
+                    <>
+                        <HelperSpan /><StyledCustomAvatar src={url} />
+                    </>
+                )
+            } else {
+                return (
+                    <>
+                        <HelperSpan /><StyledDefaultAvatar src={defaultAvatar} />
+                    </>
+                )
+            }
         } else {
             return (
                 <>
