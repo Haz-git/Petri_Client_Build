f695f659fe11b76c34b6cdea741336f284102abf
Haz-git <zhouharry1@gmail.com>
2020-12-11 08:43:43 -0800

    Added action creator to remove persist:root, as well as a line to clear local storage

--- frontend/src/utils/logouthelper.js
+++ frontend/src/utils/logouthelper.js
@@ -1,8 +1,12 @@
 import history from '../historyObject';
+import { userLogOut } from '../redux/logout_action';
 
 export const logouthelper = () => {
     //Remove JWT.
     localStorage.removeItem('jwt');
+    localStorage.removeItem('persist:root');
+    localStorage.clear();
+    userLogOut();
     console.log('User has logged out successfully');
     window.location.reload(true);
 
