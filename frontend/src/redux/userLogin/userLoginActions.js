fe5396222cb45ceeadb217bd57df3d34ecded8ce
Haz-git <zhouharry1@gmail.com>
2020-12-13 20:52:23 -0800

    Added return flag and try/catch blocks to action creator

--- frontend/src/redux/userLogin/userLoginActions.js
+++ frontend/src/redux/userLogin/userLoginActions.js
@@ -5,31 +5,45 @@ import api from '../../api';
 const userLogin = formValues => async dispatch => {
 
     //Push the user to a loading page for confirmation of Login...:
-    history.push('/loading');
 
     // Send a POST request to api:
-    const response = await api.post('/users/login', {...formValues});
 
-    //Store JWT in response into localstorage:
+    let response; 
 
     try {
-        localStorage.setItem('jwt', JSON.stringify(response.data));
-    } catch (e) {
-        if (e.name === 'QuotaExceededError') {
-            localStorage.clear();
-            localStorage.setItem('jwt', JSON.stringify(response.data));
-        } else {
-            alert('Your local storage seems to be full...');
+        response = await api.post('/users/login', {...formValues});
+    } catch (error) {
+        if (error) {
+            const errorFlag = true;
+            return errorFlag;
         }
     }
 
-    //Dispatch response object to reducers:
-    dispatch({
-        type: USER_LOG_IN,
-        payload: response.data,
-    })
+    history.push('/loading');
 
-    history.push('/dashboard');
+    // Store JWT in response into localstorage:
+    setTimeout(() => {
+        
+        try {
+            localStorage.setItem('jwt', JSON.stringify(response.data));
+        } catch (e) {
+            if (e.name === 'QuotaExceededError') {
+                localStorage.clear();
+                localStorage.setItem('jwt', JSON.stringify(response.data));
+            } else {
+                alert('Your local storage seems to be full...');
+            }
+        }
+
+        //Dispatch response object to reducers:
+        dispatch({
+            type: USER_LOG_IN,
+            payload: response.data,
+        })
+
+        history.push('/dashboard');
+
+    }, 5000);
 
 }
 
