e9e8d621652e23f4bff90536f84c221582665dd6
Haz-git <zhouharry1@gmail.com>
2020-12-16 09:44:50 -0800

    Creating light and dark themes

--- frontend/src/components/Styling/Theme.js
+++ frontend/src/components/Styling/Theme.js
@@ -3,11 +3,23 @@
 export const lightTheme = {
     mainBody: '#f6f9fc',
     text: '#1c1e37',
-    background: '#ffffff',
+    background: '#f6f9fc',
+    navBgColor: '#f6f9fc',
+    navBorderColor: '1px solid white',
+    LogoDividerColor: '#ccc',
+    navIconColor: '#2e3054',
+    navIconHoverColor: 'red',
+    mdTodoBgColor: 'white',
 }
 
 export const darkTheme = {
     mainBody: '#202345',
     text: '#ffffff',
-    background: '#4D5075',
+    background: '#1c1e37',
+    navBgColor: '#222444',
+    navBorderColor: '1px solid black',
+    LogoDividerColor: 'black',
+    navIconColor: '#ffffff',
+    navIconHoverColor: '#ADD6FF',
+    mdTodoBgColor: '#2d2f57',
 }
