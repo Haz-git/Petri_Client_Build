1b7668f8f3f909facd9c8995ec4840f5217eb0a6
Haz-git <zhouharry1@gmail.com>
2020-12-16 13:38:04 -0800

    Adding more theme styles

--- frontend/src/components/Styling/Theme.js
+++ frontend/src/components/Styling/Theme.js
@@ -4,22 +4,34 @@ export const lightTheme = {
     mainBody: '#f6f9fc',
     text: '#1c1e37',
     background: '#f6f9fc',
+    //Main Dashboard
     navBgColor: '#f6f9fc',
     navBorderColor: '1px solid white',
     LogoDividerColor: '#ccc',
     navIconColor: '#2e3054',
     navIconHoverColor: 'red',
     mdTodoBgColor: 'white',
+    //Messenger
+    chatContainerBGColor: 'white',
+    chatInputContainer: 'white',
+    chatInputColor: '#293241',
+    chatInputText: 'white',
 }
 
 export const darkTheme = {
     mainBody: '#202345',
     text: '#ffffff',
-    background: '#1c1e37',
+    background: '#141628',
+    //Main DashBoard
     navBgColor: '#222444',
     navBorderColor: '1px solid black',
     LogoDividerColor: 'black',
     navIconColor: '#ffffff',
     navIconHoverColor: '#ADD6FF',
     mdTodoBgColor: '#2d2f57',
+    //Messenger
+    chatContainerBGColor: '#2d2f57',
+    chatInputContainer: '#2d2f57',
+    chatInputColor: 'white',
+    chatInputText: '#293241',
 }
