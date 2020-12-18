89a9e3b744783d54a45ab6baf17646db3364ff89
Haz-git <zhouharry1@gmail.com>
2020-12-18 13:21:34 -0800

    Creating graphic

---
+++ frontend/src/components/landingPage/BackdropGraphic.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+//Styles:
+
+const MainContainer = styled.div`
+    position: relative;
+    background-color: salmon;
+    width: 500px;
+    height: 600px;
+    perspective: 700px;
+
+`
+
+
+
+
+
+
+
+
+
+//Render:
+
+const BackdropGraphic = () => {
+    return (
+        <>
+            <MainContainer>
+
+            </MainContainer>
+        </>
+    )
+}
+
+export default BackdropGraphic;
