82550a458d56f227b675f1ff7e78a52651d6cc98
Haz-git <zhouharry1@gmail.com>
2020-12-21 14:40:49 -0800

    Added basic footer

--- frontend/src/components/landingPage/Footer.js
+++ frontend/src/components/landingPage/Footer.js
@@ -1,18 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+//Styles:
+
+import { GithubSquare } from '@styled-icons/fa-brands/GithubSquare';
 
 const FooterDiv = styled.div`
-    background-color: lightcoral;
+    display: flex;
+    background: radial-gradient(circle, rgba(54,11,66,1) 9%, rgba(42,5,51,1) 38%, rgba(41,5,50,1) 69%, rgba(27,0,31,1) 100%);
     text-align: center;
-`
-// const FooterHeader = styled.h2`
+    margin: 0;
+    border-top: 1px solid white;
+    padding: 40px 40px;
+    justify-content: space-between;
+    align-items: center;
 
-// `
+`
+const FooterHeader = styled.h2`
+    margin: 0;
+    font-family: 'Nunito', sans-serif;
+    color: white;
+    font-size: 17px;
+    font-weight: 100;
+`
+const StyledAnchor = styled.a`
+    text-decoration: none;
+    color: white;
+    transition: all .5s ease-in;
+
+    &:hover {
+        text-decoration: none;
+        color: white;
+        transform: scale(1.1);
+    }
+`
+
+const GithubIcon = styled(GithubSquare)`
+    height: 50px;
+    width: 50px;
+`
+
+
+//Render:
 
 const Footer = () => {
     return (
         <FooterDiv>
-            <h2>Test it out! Login or Register!</h2>
+            <FooterHeader>Made by Harry Zhou <span>&copy;</span> 2020.</FooterHeader>
+            <FooterHeader>haz.dev.git@gmail.com (For Business Purposes Only)</FooterHeader>
+            <FooterHeader>
+                <StyledAnchor target='_blank' href='https://github.com/Haz-git'>
+                    <GithubIcon />
+                </StyledAnchor>
+            </FooterHeader>
         </FooterDiv>
     )
 }
