14d308c5e7acb45741d971336269900c5a8319b5
Haz-git <zhouharry1@gmail.com>
2020-12-22 13:52:24 -0800

    Added attachment of JWT header

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,23 @@
 import axios from 'axios';
+import { getJWT } from '../utils/jwthelper';
+
+//Grab JWT from localstorage, send as request header to backend.
+const localStorageItem = getJWT();
+let JWT;
+
+//Check if the JWT is properly located within 'token' of the localStorageItem
+
+if (localStorageItem !== null && localStorageItem !== undefined) {
+    if (localStorageItem.token !== undefined) {
+        JWT = localStorageItem.token;
+    }
+}
 
 export default axios.create({
-    // baseURL: 'http://localhost:8080/api',
-    baseURL: 'https://petri-webapp-heroku.herokuapp.com/api',
+    baseURL: 'http://localhost:8080/api',
+    // baseURL: 'https://petri-webapp-heroku.herokuapp.com/api',
     withCredentials: true,
+    headers: {
+        'Authorization': `Bearer ${JWT}`,
+    }
 });
