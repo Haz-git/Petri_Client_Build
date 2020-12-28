405884e82d546f9dad554b53c0b61cf977044379
Haz-git <zhouharry1@gmail.com>
2020-12-28 09:19:01 -0800

    Refactored index API

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,25 +1,31 @@
 import axios from 'axios';
-import { getJWT } from '../utils/jwthelper';
 
-//Grab JWT from localstorage, send as request header to backend.
-const localStorageItem = getJWT();
-let JWT;
+const createAxiosInstanceWithHeader = () => {
+    const localStorageItem = JSON.parse(localStorage.getItem('jwt'));
 
-//Check if the JWT is properly located within 'token' of the localStorageItem
 
-if (localStorageItem !== null && localStorageItem !== undefined) {
-    if (localStorageItem.token !== undefined) {
-        JWT = localStorageItem.token;
+    if (localStorageItem !== null && localStorageItem !== undefined) {
+        if (localStorageItem.token !== undefined) {
+            const JWT = localStorageItem.token;
+
+            //Bearer + JWT has been attached to the header of the axios instance--this is used for the authenticateJWT protection middleware in the server for editing actions, but not for data retrieval.
+
+            const axiosInstanceHeader = axios.create({
+                baseURL: 'http://localhost:8080/api',
+                // baseURL: 'https://petri-webapp-heroku.herokuapp.com/api',
+                withCredentials: true,
+                headers: {
+                    'Authorization': `Bearer ${JWT}`,
+                }
+            });
+
+            return axiosInstanceHeader;
+        }
     }
+
 }
 
-//Bearer + JWT has been attached to the header of the axios instance--this is used for the authenticateJWT protection middleware in the server.
+const axiosInstance = createAxiosInstanceWithHeader();
 
-export default axios.create({
-    baseURL: 'http://localhost:8080/api',
-    // baseURL: 'https://petri-webapp-heroku.herokuapp.com/api',
-    withCredentials: true,
-    headers: {
-        'Authorization': `Bearer ${JWT}`,
-    }
-});
+
+export default axiosInstance;
