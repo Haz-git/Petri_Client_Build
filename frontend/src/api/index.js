8a409c39c7de199fbf562ce6f3cfce77b29a6df3
Haz-git <zhouharry1@gmail.com>
2020-12-29 09:29:24 -0800

    Added clean JWT to header

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,31 +1,12 @@
 import axios from 'axios';
 
-const createAxiosInstanceWithHeader = () => {
-    const localStorageItem = JSON.parse(localStorage.getItem('jwt'));
 
+export default axios.create({
+    baseURL: 'http://localhost:8080/api',
+    // baseURL: 'https://petri-webapp-heroku.herokuapp.com/api',
+    withCredentials: true,
+    headers: {
+        'Authorization': "Bearer " + JSON.parse(localStorage.getItem('jwt')),
+    }   
+})
 
-    if (localStorageItem !== null && localStorageItem !== undefined) {
-        if (localStorageItem.token !== undefined) {
-            const JWT = localStorageItem.token;
-
-            //Bearer + JWT has been attached to the header of the axios instance--this is used for the authenticateJWT protection middleware in the server for editing actions, but not for data retrieval.
-
-            const axiosInstanceHeader = axios.create({
-                baseURL: 'http://localhost:8080/api',
-                // baseURL: 'https://petri-webapp-heroku.herokuapp.com/api',
-                withCredentials: true,
-                headers: {
-                    'Authorization': `Bearer ${JWT}`,
-                }
-            });
-
-            return axiosInstanceHeader;
-        }
-    }
-
-}
-
-const axiosInstance = createAxiosInstanceWithHeader();
-
-
-export default axiosInstance;
