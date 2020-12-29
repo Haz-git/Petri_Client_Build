eb3eaff90bfb7f95d74683cc4b35bddd0540b1cd
Haz-git <zhouharry1@gmail.com>
2020-12-29 13:31:05 -0800

    Refactored css to be more mobile friendly and added window event listener to prevent component reverse

--- frontend/src/components/landingPage/BodyBlock.js
+++ frontend/src/components/landingPage/BodyBlock.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Fade from 'react-reveal/Fade';
 
@@ -8,6 +8,10 @@ const BlockDetailMain = styled.div`
     display: grid;
     grid-template-columns: 50% 50%;
     align-items: center;
+
+    @media only screen and (max-width: 600px) {
+        display: block;
+    }
 `
 const BlockDetailImg = styled.div`
     display: inline-block;
@@ -34,6 +38,10 @@ const BlockDetailHeader = styled.h2`
     border-radius: 10px;
     box-shadow: 0 1px 3px rgba(0, 0, 0,0.12), 0 1px 2px rgba(0,0,0.24);
 
+    @media only screen and (max-width: 600px) {
+        font-size: 44px;
+    }
+
 `
 
 const BlockDetailDesc = styled.p`
@@ -45,6 +53,10 @@ const BlockDetailDesc = styled.p`
     text-align: justify;
     padding-left: 45px;
     padding-right: 45px;
+
+    @media only screen and (max-width: 600px) {
+        font-size: 30px;
+    }
 `
 
 
@@ -57,6 +69,10 @@ const StyledImg = styled.img`
     max-height: 100%;
     border-radius: 10px;
     box-shadow: 0 -2px 10px rgba(0, 0, 0, 1);
+
+    @media only screen and (max-width: 600px) {
+        margin-top: 20px;
+    }
 `
 
 //Helper Functions:
@@ -82,16 +98,52 @@ const BodyBlock = ({Title, Description, Img, Reverse}) => {
 
     /*
         This is a very reusable component designed for displaying/advertising the app's features. The props are editable in the MainBackdrop. The 'Reverse' prop simple returns a version where the image and text are reversed. This is for aesthetic purposes.
+
+        There is also an attached event listener to the window's viewwidth in order to make the website a bit more mobile friendly. If the viewWidth is less than 600, then the components will NOT be reversed because the display is no longer grid--it's block.
     */
 
+    const [ viewWidth, setViewWidth ] = useState(0);
+    const [ viewHeight, setViewHeight ] = useState(0);
+
+    useEffect(() => {
+        window.addEventListener('resize', updateWindowSettings);
+
+        return () => window.removeEventListener('resize', updateWindowSettings);
+    }, [])
+
+    const updateWindowSettings = () => {
+        setViewHeight(window.innerHeight);
+        setViewWidth(window.innerWidth);
+    }
+
     if (Reverse === 'true') {
-        return (
-            <Fade>
-                <BlockDetailMain>
-                    {reverseComponent(Title, Description, Img)}
-                </BlockDetailMain>
-            </Fade>
-        )
+        if (viewWidth <= 600 && viewWidth !== 0 || window.innerWidth <= 600) {
+            return (
+                <Fade>
+                    <BlockDetailMain>
+                        <BlockDetailText>
+                            <HeaderDecoration>
+                                <BlockDetailHeader>{Title}</BlockDetailHeader>
+                            </HeaderDecoration>
+                            <BlockDetailDesc>{Description}</BlockDetailDesc>
+                        </BlockDetailText>
+                        <BlockDetailImg>
+                            <ImgContainer>
+                                <StyledImg src={Img}></StyledImg>
+                            </ImgContainer>
+                        </BlockDetailImg>
+                    </BlockDetailMain>
+                </Fade>
+            )
+        } else {
+            return (
+                <Fade>
+                    <BlockDetailMain>
+                        {reverseComponent(Title, Description, Img)}
+                    </BlockDetailMain>
+                </Fade>
+            )
+        }
     }
 
     return (
