60c313b2488821cffc7a0401762c7c6cddc2ae81
Haz-git <zhouharry1@gmail.com>
2021-06-24 09:24:18 -0700

    Failing to compile on implicit type

---
+++ frontend/src/redux/userTaskLog/userTaskLogReducer.ts
@@ -0,0 +1,21 @@
+import { Action } from 'redux';
+import {
+    USER_NEW_TASK,
+    USER_GET_TASKS,
+    USER_DELETED_TASK,
+} from './userTaskLogTypes';
+
+const initialState = {};
+
+export default (state = initialState, action: Action) => {
+    switch (action.type) {
+        case USER_NEW_TASK:
+            return { ...state, Tasks: action.payload };
+        case USER_GET_TASKS:
+            return { ...state, Tasks: action.payload };
+        case USER_DELETED_TASK:
+            return { ...state, Tasks: action.payload };
+        default:
+            return state;
+    }
+};
--- frontend/src/redux/userTaskLog/userTaskLogTypes.js
+++
@@ -1,3 +0,0 @@
-export const USER_NEW_TASK = 'USER_NEW_TASK';
-export const USER_GET_TASKS = 'USER_GET_TASKS';
-export const USER_DELETED_TASK = 'USER_DELETED_TASK';
