35f493fade0a6c19ef139b5c823b847b4aba3fce
Haz-git <zhouharry1@gmail.com>
2021-06-25 08:27:13 -0700

    Fixed JWT check

--- frontend/src/components/Dashboard/homePage/MainDashboard.js
+++ frontend/src/components/Dashboard/homePage/MainDashboard.js
@@ -15,30 +15,29 @@ import defaultAvatar from '../../../Img/default_avatar.png';
 const WrapperContainer = styled.div`
     overflow-y: hidden;
     height: 100%;
-`
+`;
 
 const SpinnerContainer = styled.div`
     position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-`
-
+`;
 
 const MainDashboardContainer = styled.div`
     height: 100%;
-    background-color: ${props => props.theme.background};
+    background-color: ${(props) => props.theme.background};
     z-index: 0;
     overflow-y: hidden;
     overflow: hidden;
-`
+`;
 const MainDashboardHeaderContainer = styled.div`
     padding-left: 40px;
     display: flex;
     text-align: center;
-    background-color: ${props => props.theme.settingsHeaderBG};
+    background-color: ${(props) => props.theme.settingsHeaderBG};
     height: 86px;
-    border-left: 1px solid #F6F9FC;
+    border-left: 1px solid #f6f9fc;
     justify-content: flex-start;
     align-items: center;
     margin: 0;
@@ -46,64 +45,64 @@ const MainDashboardHeaderContainer = styled.div`
     padding-bottom: 0;
     border-bottom: 1px solid #141628;
     overflow-y: hidden;
-`
+`;
 const MainDashboardUserNameHeader = styled.h1`
     font-family: 'Montserrat', sans-serif;
     margin: 0;
     font-size: 25px;
     font-weight: 500;
-    color: ${props => props.theme.navDetailUserName};
+    color: ${(props) => props.theme.navDetailUserName};
     display: flex;
     justify-content: flex-start;
     padding-left: 15px;
     padding-bottom: 0px;
     padding-top: 10px;
-`
+`;
 
 const MainDashboardHeader = styled.h2`
     font-family: 'Montserrat', sans-serif;
     margin: 0;
     font-size: 16px;
     font-weight: 100;
-    color: ${props => props.theme.navDetailName};
+    color: ${(props) => props.theme.navDetailName};
     display: flex;
     justify-content: flex-start;
     padding-left: 15px;
     padding-bottom: 0px;
-`
+`;
 const MainDashboardHeader2 = styled.h2`
     font-family: 'Montserrat', sans-serif;
     padding-top: 0px;
     margin: 0;
     font-size: 12px;
     font-weight: 100;
-    color: ${props => props.theme.navDetailEmail};
+    color: ${(props) => props.theme.navDetailEmail};
     display: flex;
     justify-content: flex-start;
     padding-left: 15px;
     padding-bottom: 0px;
-`
+`;
 
 const MainDashboardUpperGrid = styled.div`
-    background-color: ${props => props.theme.background};
+    background-color: ${(props) => props.theme.background};
     padding: 20px 20px;
     display: grid;
     grid-template-columns: 35% 65%;
     z-index: 0;
-`
+`;
 
 const UpperGridContainer = styled.div`
     text-align: center;
     padding: 10px 10px;
-`
+`;
 
 const StyledCustomAvatar = styled.img`
     height: 65px;
     width: 65px;
     border-radius: 50%;
     vertical-align: middle;
-    border: 2px solid ${props => props.theme.navAvatarBorder};
-`
+    border: 2px solid ${(props) => props.theme.navAvatarBorder};
+`;
 
 const StyledDefaultAvatar = styled.img`
     height: 65px;
@@ -111,14 +110,13 @@ const StyledDefaultAvatar = styled.img`
     background-color: white;
     border-radius: 50%;
     vertical-align: middle;
-    border: 2px solid ${props => props.theme.navAvatarBorder};
-    
-`
+    border: 2px solid ${(props) => props.theme.navAvatarBorder};
+`;
 const HelperSpan = styled.span`
     display: inline-block;
     vertical-align: middle;
     height: 100%;
-`
+`;
 
 const DetailsContainer = styled.div`
     margin: 0;
@@ -127,42 +125,39 @@ const DetailsContainer = styled.div`
     margin-bottom: 24px;
     padding-top: 10px;
     overflow-y: hidden;
-`
+`;
 
 class MainDashboard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading: null
+            loading: null,
         };
     }
 
     componentDidMount() {
-
-        if (this.props.userDetails === undefined || this.props.userDetails === null) {
-
+        if (
+            this.props.userDetails === undefined ||
+            this.props.userDetails === null
+        ) {
             this.setState({ loading: true }, () => {
-                this.props.userGetProfilePicture()
-                    .then(result => {
+                this.props.userGetProfilePicture().then((result) => {
+                    const { _id, firstName, lastName, userName, email } =
+                        this.props.userDetails;
 
-                        const { _id, firstName, lastName, userName, email } = this.props.userDetails;
-
-                        this.setState({
-                            _id,
-                            firstName,
-                            lastName,
-                            userName,
-                            email,
-                            loading: result,
-                        })
-
-
-                    })
-            })
-
+                    this.setState({
+                        _id,
+                        firstName,
+                        lastName,
+                        userName,
+                        email,
+                        loading: result,
+                    });
+                });
+            });
         } else {
-
-            const { _id, firstName, lastName, userName, email } = this.props.userDetails;
+            const { _id, firstName, lastName, userName, email } =
+                this.props.userDetails;
 
             this.setState({
                 _id,
@@ -171,49 +166,57 @@ class MainDashboard extends Component {
                 userName,
                 email,
                 loading: false,
-            })
+            });
         }
     }
 
     renderDashBoardProfilePicture = () => {
-        if (this.props.userDetails.profileImg.url !== undefined && this.props.userDetails.profileImg.url !== null) {
+        if (
+            this.props.userDetails.profileImg.url !== undefined &&
+            this.props.userDetails.profileImg.url !== null
+        ) {
             const { url, constraints } = this.props.userDetails.profileImg;
-            
+
             return (
                 <>
-                    <HelperSpan /><StyledCustomAvatar src={url} />
+                    <HelperSpan />
+                    <StyledCustomAvatar src={url} />
                 </>
-            )
+            );
         } else {
             return (
                 <>
-                    <HelperSpan /><StyledDefaultAvatar src={defaultAvatar} />
+                    <HelperSpan />
+                    <StyledDefaultAvatar src={defaultAvatar} />
                 </>
-            )
+            );
         }
-    }
-
+    };
 
     render() {
-
         if (this.state.loading === false) {
-
             const { firstName, lastName, userName, email } = this.state;
 
             return (
                 <>
                     <WrapperContainer>
                         <MainDashboardHeaderContainer>
-                        <Fade>
-                            <div>
-                                {this.renderDashBoardProfilePicture()}
-                            </div>
-                            <DetailsContainer>
-                                <MainDashboardUserNameHeader>{userName} </MainDashboardUserNameHeader>
-                                <MainDashboardHeader>{firstName}, {lastName} </MainDashboardHeader>
-                                <MainDashboardHeader2>{email}</MainDashboardHeader2>
-                            </DetailsContainer>
-                        </Fade>
+                            <Fade>
+                                <div>
+                                    {/* {this.renderDashBoardProfilePicture()} */}
+                                </div>
+                                <DetailsContainer>
+                                    <MainDashboardUserNameHeader>
+                                        {userName}{' '}
+                                    </MainDashboardUserNameHeader>
+                                    <MainDashboardHeader>
+                                        {firstName}, {lastName}{' '}
+                                    </MainDashboardHeader>
+                                    <MainDashboardHeader2>
+                                        {email}
+                                    </MainDashboardHeader2>
+                                </DetailsContainer>
+                            </Fade>
                         </MainDashboardHeaderContainer>
                         <MainDashboardContainer>
                             <MainDashboardUpperGrid>
@@ -227,7 +230,7 @@ class MainDashboard extends Component {
                         </MainDashboardContainer>
                     </WrapperContainer>
                 </>
-            )
+            );
         } else {
             return (
                 <>
@@ -235,16 +238,17 @@ class MainDashboard extends Component {
                         <CircularProgress />
                     </SpinnerContainer>
                 </>
-            )
+            );
         }
     }
 }
 
-const mapStateToProps = state => {
-
+const mapStateToProps = (state) => {
     return {
         userDetails: state.userSettings.userSettings,
-    }
-}
+    };
+};
 
-export default connect(mapStateToProps, { userGetProfilePicture })(MainDashboard);
+export default connect(mapStateToProps, { userGetProfilePicture })(
+    MainDashboard
+);
