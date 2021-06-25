c622b12ef185e8c7fcefabce93f698104b4a2fc4
Haz-git <zhouharry1@gmail.com>
2021-06-25 08:48:35 -0700

    Added try catch block

--- frontend/src/redux/userSettings/UserSettingActions.js
+++ frontend/src/redux/userSettings/UserSettingActions.js
@@ -1,5 +1,5 @@
 import api from '../../api/index';
-import { 
+import {
     USER_ADD_NEW_PROFILE_PICTURE,
     USER_GET_PROFILE_PICTURE,
     USER_CHANGE_EMAIL_ADDRESS,
@@ -11,44 +11,76 @@ import historyObject from '../../historyObject';
 
 export function userGetProfilePicture() {
     return async (dispatch, getState) => {
-        const { auth: { userLogIn: { data: { _id } }} } = getState();
+        try {
+            const {
+                auth: {
+                    userLogIn: {
+                        data: { _id },
+                    },
+                },
+            } = getState();
 
-        const response = await api.post('/users/settings/getProPic', { _id });
-        
-        dispatch({
-            type: USER_GET_PROFILE_PICTURE,
-            payload: response.data.existingUser
-        })
+            const response = await api.post('/users/settings/getProPic', {
+                _id,
+            });
+
+            dispatch({
+                type: USER_GET_PROFILE_PICTURE,
+                payload: response.data.existingUser,
+            });
+        } catch (e) {
+            console.log(e);
+        }
+
         //This should render loading == false in main dashboard.
         return false;
-    }
+    };
 }
 
 export function userAddNewProfilePicture(imgURL, imgConstraints) {
     return async (dispatch, getState) => {
-        const { auth: { userLogIn: { data: { _id } }} } = getState();
+        const {
+            auth: {
+                userLogIn: {
+                    data: { _id },
+                },
+            },
+        } = getState();
 
-        const response = await api.post('/users/settings/setNewProPic', { _id, imgURL, imgConstraints });
+        const response = await api.post('/users/settings/setNewProPic', {
+            _id,
+            imgURL,
+            imgConstraints,
+        });
 
         dispatch({
             type: USER_ADD_NEW_PROFILE_PICTURE,
             payload: response.data.responseUpdatedProfilePic,
-        })
+        });
 
         historyObject.push('/settings');
-    }
+    };
 }
 
 export function userChangeLastName(newLastName) {
     return async (dispatch, getState) => {
-        const { auth: { userLogIn: { data: { _id } }} } = getState();
+        const {
+            auth: {
+                userLogIn: {
+                    data: { _id },
+                },
+            },
+        } = getState();
 
-        const response = await api.post('/users/settings/changeLastName', { _id, newLastName });
+        const response = await api.post('/users/settings/changeLastName', {
+            _id,
+            newLastName,
+        });
 
         dispatch({
             type: USER_CHANGE_LASTNAME,
-            payload: response.data.updatedLastNameUser
-        })
+            payload: response.data.updatedLastNameUser,
+        });
 
         /*Notes:
 
@@ -62,44 +94,71 @@ export function userChangeLastName(newLastName) {
 
 
         */
-    }
+    };
 }
 
 export function userChangeFirstName(newFirstName) {
     return async (dispatch, getState) => {
-        const { auth: { userLogIn: { data: { _id } }} } = getState();
+        const {
+            auth: {
+                userLogIn: {
+                    data: { _id },
+                },
+            },
+        } = getState();
 
-        const response = await api.post('/users/settings/changeFirstName', { _id, newFirstName });
+        const response = await api.post('/users/settings/changeFirstName', {
+            _id,
+            newFirstName,
+        });
 
         dispatch({
             type: USER_CHANGE_FIRSTNAME,
-            payload: response.data.updatedFirstNameUser
-        })
-    }
+            payload: response.data.updatedFirstNameUser,
+        });
+    };
 }
 
 export function userChangeUserName(newUserName) {
     return async (dispatch, getState) => {
-        const { auth: { userLogIn: { data: { _id } }} } = getState();
+        const {
+            auth: {
+                userLogIn: {
+                    data: { _id },
+                },
+            },
+        } = getState();
 
-        const response = await api.post('/users/settings/changeUserName', { _id, newUserName });
+        const response = await api.post('/users/settings/changeUserName', {
+            _id,
+            newUserName,
+        });
 
         dispatch({
             type: USER_CHANGE_USERNAME,
-            payload: response.data.updatedUserNameUser
-        })
-    }
+            payload: response.data.updatedUserNameUser,
+        });
+    };
 }
 
 export function userChangeEmailAddress(newEmailAddress) {
     return async (dispatch, getState) => {
-        const { auth: { userLogIn: { data: { _id } }} } = getState();
+        const {
+            auth: {
+                userLogIn: {
+                    data: { _id },
+                },
+            },
+        } = getState();
 
-        const response = await api.post('/users/settings/changeEmailAddress', { _id, newEmailAddress });
+        const response = await api.post('/users/settings/changeEmailAddress', {
+            _id,
+            newEmailAddress,
+        });
 
         dispatch({
             type: USER_CHANGE_EMAIL_ADDRESS,
-            payload: response.data.updatedEmailAddressUser
-        })
-    }
+            payload: response.data.updatedEmailAddressUser,
+        });
+    };
 }
