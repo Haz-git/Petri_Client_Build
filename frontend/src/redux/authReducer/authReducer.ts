dee0cbc0d2463f0ed6617401dc3dd90ca3390a44
Haz-git <zhouharry1@gmail.com>
2021-06-26 06:42:08 -0700

    Configured reducer for TS

--- frontend/src/redux/authReducer/authReducer.ts
+++ frontend/src/redux/authReducer/authReducer.ts
@@ -1,5 +1,3 @@
-import { USER_LOG_IN } from '../userLogin/userLoginTypes';
-
 //Action Interfaces and action-types for userSignUp functions:
 import { UserSignUpActionInterface } from '../userSignUp/userSignUpInterfaces';
 import { SignUpActionType } from '../userSignUp/action-types';
@@ -10,12 +8,17 @@ import { LogInActionType } from '../userLogin/action-types';
 
 const initialState = {};
 
+//Because we'll need the Action Interfaces from both userSignUp and UserLogIn, we'll create an intersection interface extending both action interfaces. Since type can be from UserSignUp or UserLogIn, we'll use any.
+interface AuthActionInterface
+    extends UserSignUpActionInterface,
+        UserLogInActionInterface {
+    type: any;
+    payload?: any;
+}
+
 //This reducer handles user login and user sign up authentication.
 
-export default (
-    state: Object = initialState,
-    action: UserSignUpActionInterface & UserLogInActionInterface
-) => {
+export default (state: Object = initialState, action: AuthActionInterface) => {
     switch (action.type) {
         case SignUpActionType.USER_SIGN_UP:
             return { ...state, userSignUp: action.payload };
