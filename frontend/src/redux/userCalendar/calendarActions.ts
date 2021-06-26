d03f3e5e7225f73ecf973f5460bb7a574bac97cf
Haz-git <zhouharry1@gmail.com>
2021-06-26 13:24:13 -0700

    Changed to TS file

---
+++ frontend/src/redux/userCalendar/calendarActions.ts
@@ -0,0 +1,69 @@
+import api from '../../api/index';
+import {
+    USER_ADD_EVENT,
+    USER_GET_EVENTS,
+    USER_UPDATE_EVENT,
+    USER_DELETE_EVENT,
+} from './calendarTypes';
+
+export function addNewEvent(event) {
+    return async (dispatch, getState) => {
+        const { auth: { userLogIn: { data: { _id } } } } = getState();
+
+        const response = await api.post('/users/calendar/new', { _id, event });
+
+        //The following state dispatches are commented out due to fullcalendar's state conflicting with the redux state. Not dispatching a new state to re-render the fullCalendar component prevents an additional render, therefore preventing showing a user's dragged events twice.
+
+        // dispatch({
+        //     type: USER_ADD_EVENT,
+        //     payload: response.data.userNewCalendarEvents,
+        // });
+    }
+}
+
+export function getEvents() {
+    return async (dispatch, getState) => {
+        const { auth: { userLogIn: { data: { _id } } } } = getState();
+
+        const response = await api.post('/users/calendar/events', { _id });
+
+        dispatch({
+            type: USER_GET_EVENTS,
+            payload: response.data.existingUserCalendarEvents
+        });
+
+        //Returning flag for loading finish: 
+        return false;
+
+    }
+}
+
+export function deleteEvent(event) {
+    return async (dispatch, getState) => {
+        const { auth: { userLogIn: { data: { _id } } } } = getState();
+
+        const response = await api.post('/users/calendar/delete', { event, _id });
+
+        console.log(response);
+
+        // dispatch({
+        //     type: USER_DELETE_EVENT,
+        //     payload: response.data.updatedDeletedCalendarEvents
+        // });
+    }
+}
+
+export function updateEvent(event) {
+    return async (dispatch, getState) => {
+        const { auth: { userLogIn: { data: { _id } } } } = getState();
+
+        const response = await api.patch('/users/calendar/update', { event, _id });
+
+        //This dispatch is commented out for the same reason above.
+
+        // dispatch({
+        //     type: USER_UPDATE_EVENT,
+        //     payload: response.data.updatedCalendarEvents
+        // });
+    }
+}
