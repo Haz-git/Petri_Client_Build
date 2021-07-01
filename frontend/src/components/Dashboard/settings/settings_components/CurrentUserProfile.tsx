1b62de6fc49c3411e64bd90069d60f4c8bad243d
Haz-git <zhouharry1@gmail.com>
2021-07-01 11:04:52 -0700

    Added new component for rendering current user details

--- frontend/src/components/Dashboard/settings/settings_components/CurrentUserProfile.tsx
+++ frontend/src/components/Dashboard/settings/settings_components/CurrentUserProfile.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
+/*
+    This component should recieve detailed props from parent--which is the UserSettings view, that would render the current profile picture, and current user details save for the password.
+*/
+
 const CurrentUserProfile = () => {
     return <div></div>;
 };
