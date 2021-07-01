167579cdbf109cea41df0fbac76e54e8f900322c
Haz-git <zhouharry1@gmail.com>
2021-07-01 09:46:26 -0700

    Added padding to body

--- frontend/src/components/Styling/GlobalStyles.ts
+++ frontend/src/components/Styling/GlobalStyles.ts
@@ -1,5 +1,5 @@
 //Creating Global Style:
-import { createGlobalStyle} from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 //Trying to create two global styles.. one for the landing page and one for the dashboard.
 //It seems as though whoever is nested higher takes precedent and overrules the lower level.
@@ -13,7 +13,7 @@ export const DashboardGlobalStyles = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.background};
     min-height: 100%;
+    margin-left: 100px;
   }
 
-`
-
+`;
