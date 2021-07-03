295b8cdcc5415164c95824304915003055d736ef
Haz-git <zhouharry1@gmail.com>
2021-07-03 10:06:30 -0700

    Created component for password change

---
+++ frontend/src/components/Dashboard/settings/settings_components/PasswordDetails.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+//Styles:
+
+//Interface:
+
+const PasswordDetails = () => {
+    return <div></div>;
+};
+
+export default PasswordDetails;
