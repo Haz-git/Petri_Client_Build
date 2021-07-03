e9aa9bfb5db7094d643e20c6dac2ac1999df3313
Haz-git <zhouharry1@gmail.com>
2021-07-03 14:55:27 -0700

    Added styling and forms to passwrod details

--- frontend/src/components/Dashboard/settings/settings_components/PasswordDetails.tsx
+++ frontend/src/components/Dashboard/settings/settings_components/PasswordDetails.tsx
@@ -1,12 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
 
+//Components:
+
+import {
+    MainContainer,
+    FormHeader,
+    FormContainer,
+    TextFieldContainer,
+    ButtonContainer,
+    ButtonSpacer,
+} from './ProfileDetails';
+import SettingsInputTextField from './SettingsInputTextField';
+import GeneralButton from '../../general_components/GeneralButton';
+
 //Styles:
 
 //Interface:
 
 const PasswordDetails = () => {
-    return <div>This is a temporary placeholder PASSWORD PAGE</div>;
+    return (
+        <MainContainer>
+            <FormHeader>Password Management</FormHeader>
+            <FormContainer>
+                <TextFieldContainer>
+                    <SettingsInputTextField
+                        name="currentPassword"
+                        label="Current password"
+                    />
+                </TextFieldContainer>
+                <TextFieldContainer>
+                    <SettingsInputTextField
+                        name="newPassword"
+                        label="New password"
+                    />
+                </TextFieldContainer>
+                <TextFieldContainer>
+                    <SettingsInputTextField
+                        name="newPasswordConfirm"
+                        label="Confirm new password"
+                    />
+                </TextFieldContainer>
+            </FormContainer>
+            <ButtonContainer>
+                <GeneralButton buttonLabel="Update Password" />
+                <ButtonSpacer />
+                <GeneralButton
+                    buttonLabel="Reset"
+                    buttonBackground="rgba(0, 0, 34, 0.1)"
+                    buttonTextColor="rgba(0, 0, 34, 0.7)"
+                />
+            </ButtonContainer>
+        </MainContainer>
+    );
 };
 
 export default PasswordDetails;
