4b7bec4c2b1548c1311d1fc65426c19458053ca4
Haz-git <zhouharry1@gmail.com>
2021-07-04 07:28:18 -0700

    Removed MUI click

--- frontend/src/components/Dashboard/general_components/GeneralButton.tsx
+++ frontend/src/components/Dashboard/general_components/GeneralButton.tsx
@@ -13,6 +13,7 @@ const StyledGeneralButton = styled.button<IGeneralButtonProps>`
     color: ${(props) => props.buttonTextColor};
     box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
         rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+    overflow: hidden;
 
     transition: all 0.1s ease-in;
 
