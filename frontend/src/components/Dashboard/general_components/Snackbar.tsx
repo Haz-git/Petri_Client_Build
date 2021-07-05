53e45041f79811d1fc9fb34914592903e88bb379
Haz-git <zhouharry1@gmail.com>
2021-07-05 10:31:02 -0700

    Customized Snackbar

--- frontend/src/components/Dashboard/general_components/Snackbar.tsx
+++ frontend/src/components/Dashboard/general_components/Snackbar.tsx
@@ -3,70 +3,84 @@ import styled, { keyframes } from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleSnackbarClose } from '../../../redux/snackBar/snackBarActions';
 import { FiX } from 'react-icons/fi';
+import { CheckCircle } from '@styled-icons/bootstrap/CheckCircle';
 
 //Styles:
 
 const fadein = keyframes`
     from {
-      bottom: 0;
+      top: 0;
       opacity: 0;
     }
     to {
-      bottom: 1rem;
+      top: 1rem;
       opacity: 1;
     }
 `;
 
 const fadeout = keyframes`
     from {
-      bottom: 1rem;
+      top: 1rem;
       opacity: 1;
     }
     to {
-      bottom: 0;
+      top: 0;
       opacity: 0;
     }
 `;
 
-const Container = styled.div`
-    position: fixed;
-    z-index: 1000;
-    bottom: 1rem;
-    left: 50%;
-    transform: translateX(-50%);
-    height: auto;
-    padding: 0.625rem 1rem;
-    border-radius: 0.75rem;
-    border: transparent;
-    background-color: hsl(200, 100%, 65%);
-    color: white;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+const CheckIcon = styled(CheckCircle)`
+    height: 1.7rem;
+    width: 1.7rem;
+    color: #20861b;
+    margin-right: 0.75rem;
+`;
 
+const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    position: fixed;
+    z-index: 1000;
+    top: 1rem;
+    left: 50%;
+    transform: translateX(-50%);
+    height: auto;
+    padding: 0.8rem 0.8rem;
+    border-radius: 0.3rem;
+    border: transparent;
+    background-color: white;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
 
     animation: ${fadein} 0.5s, ${fadeout} 0.5s ${(props) => props.time};
 `;
 
-const Button = styled.button`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-left: 0.875rem;
+const Message = styled.p`
+    font-family: 'Lato', sans-serif;
+    font-size: 1em;
+    font-weight: 600;
+    color: ${(props) => props.theme.text};
+    margin: 0;
     padding: 0;
-    margin-left: 1rem;
+`;
+
+const Button = styled.button`
+    /* display: flex;
+    justify-content: center;
+    align-items: center; */
+    padding: 0;
+    margin-left: 0.75rem;
     height: 1.75rem;
     width: 1.75rem;
     text-align: center;
     border: none;
     border-radius: 50%;
     background-color: transparent;
-    color: white;
+    color: ${(props) => props.theme.text};
     cursor: pointer;
 
     &:hover {
-        background-color: hsl(200, 100%, 60%);
+        background-color: rgba(0, 0, 34, 0.1);
     }
 `;
 
@@ -93,9 +107,7 @@ const Snackbar = ({ timeout }) => {
     }
 
     useEffect(() => {
-        if (SHOW) {
-            handleTimeout();
-        }
+        if (SHOW) handleTimeout();
         return () => {
             clearTimeout(TIMER);
         };
@@ -104,7 +116,8 @@ const Snackbar = ({ timeout }) => {
     return (
         SHOW && (
             <Container time={TIME}>
-                <p>{MESSAGE}</p>
+                <CheckIcon />
+                <Message>{MESSAGE}</Message>
                 <Button onClick={handleClose}>
                     <FiX />
                 </Button>
