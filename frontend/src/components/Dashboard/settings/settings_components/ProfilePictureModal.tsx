390c3589a59efa5cd90fe1b9978ce0d8a1277630
Haz-git <zhouharry1@gmail.com>
2021-07-08 09:49:57 -0700

    Added modal styles

--- frontend/src/components/Dashboard/settings/settings_components/ProfilePictureModal.tsx
+++ frontend/src/components/Dashboard/settings/settings_components/ProfilePictureModal.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Modal from '@material-ui/core/Modal';
 
 //Styles:
+const fadein = keyframes`
+    from {
+        opacity: 0;
+        -webkit-transform: translateY(300%);
+        -ms-transform: translateY(300%);
+        transform: translateY(300%)
+    }
+
+    to {
+        -webkit-transform: translateY(-50%);
+        -ms-transform: translateY(-50%);
+        transform: translateY(-50%);
+        opacity: 1;
+    }
+`;
+
 const MainContainer = styled.div``;
 
 const ModalContainer = styled.div`
-    margin-left: 100px;
+    margin-left: calc(100px + 5rem);
+    margin-right: 5rem;
     position: relative;
-    top: 50%;
+    top: 30%;
     -webkit-transform: translateY(-50%);
     -ms-transform: translateY(-50%);
     transform: translateY(-50%);
     background-color: white;
     padding: 1rem 1rem;
     text-align: center;
+    border-radius: 0.5rem;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+        rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+
+    animation: ${fadein} 0.2s ease-in;
 `;
 
 //Interface:
