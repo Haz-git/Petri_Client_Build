5f265d61b4ce3055a607994f76de3c377321b198
Haz-git <zhouharry1@gmail.com>
2021-07-08 08:42:13 -0700

    Creating modal to edit/update profile picture

---
+++ frontend/src/components/Dashboard/settings/settings_components/ProfilePictureModal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+//Styles:
+
+//Interface:
+
+const ProfilePictureModal = () => {
+    return <div></div>;
+};
+
+export default ProfilePictureModal;
