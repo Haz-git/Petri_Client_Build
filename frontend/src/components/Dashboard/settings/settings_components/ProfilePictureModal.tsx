a1e1495669a8a20181685c64c40e56d4760f20a6
Haz-git <zhouharry1@gmail.com>
2021-07-09 09:50:53 -0700

    Attached custom upload, added drag and drop and file input support

--- frontend/src/components/Dashboard/settings/settings_components/ProfilePictureModal.tsx
+++ frontend/src/components/Dashboard/settings/settings_components/ProfilePictureModal.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 import Modal from '@material-ui/core/Modal';
 import GeneralButton from '../../general_components/GeneralButton';
+import FileInput from './FileInput';
 
 import AvatarEditor from 'react-avatar-editor';
 import DropZone from 'react-dropzone';
@@ -9,6 +10,7 @@ import DropZone from 'react-dropzone';
 //Redux
 import { connect } from 'react-redux';
 import { userAddNewProfilePicture } from '../../../../redux/userSettings/UserSettingActions';
+import { AnyAaaaRecord } from 'dns';
 
 //Styles:
 const fadein = keyframes`
@@ -33,7 +35,7 @@ const ModalContainer = styled.div`
     margin-left: calc(100px + 5rem);
     margin-right: 5rem;
     position: relative;
-    top: 20%;
+    top: 40%;
     -webkit-transform: translateY(-50%);
     -ms-transform: translateY(-50%);
     transform: translateY(-50%);
@@ -76,7 +78,20 @@ const PictureContainer = styled.div`
 `;
 
 const UploadFileContainer = styled.div`
+    display: flex;
+    align-items: center;
     margin: 0.5rem 0;
+    text-align: left;
+`;
+
+const UploadFileText = styled.p`
+    font-family: 'Lato', sans-serif;
+    font-size: 1em;
+    font-weight: 700;
+    color: ${(props) => props.theme.text};
+    opacity: 1;
+    text-align: left;
+    margin-left: 1rem;
 `;
 
 const DropContainer = styled.div``;
@@ -94,6 +109,52 @@ const ProfilePictureModal = ({
     closeFunc,
     userAddNewProfilePicture,
 }: ProfilePictureModalProps): JSX.Element => {
+    //Avatar Customization State:
+    const [avatarState, setAvatarState] = useState({
+        image: '',
+        width: 250,
+        height: 250,
+        border: 5,
+        color: [115, 120, 212, 0.4],
+        scale: 1,
+        rotate: 0,
+        borderRadius: 200,
+        position: {
+            x: 0.0,
+            y: 0.0,
+        },
+    });
+
+    //Uploaded File State:
+    const [uploadedFileName, setUploadedFileName] = useState(
+        'Uploaded: No file uploaded.'
+    );
+
+    //Dropzone image handler
+    const handleImageDrop = (dropped: any) => {
+        setAvatarState({
+            ...avatarState,
+            image: dropped[0],
+        });
+    };
+
+    //File Input handler
+
+    const handleImageUpload = (e: any) => {
+        setAvatarState({
+            ...avatarState,
+            image: e.target.files[0],
+        });
+
+        if (e.target.files[0].name.length >= 20) {
+            setUploadedFileName(
+                `Uploaded: ...${e.target.files[0].name.substr(-15)}`
+            );
+        } else {
+            setUploadedFileName(`Uploaded: ${e.target.files[0].name}`);
+        }
+    };
+
     return (
         <MainContainer>
             <Modal open={openState} onClose={closeFunc}>
@@ -106,9 +167,27 @@ const ProfilePictureModal = ({
                                 files, or click 'Upload file'.
                             </ModalDescText>
                             <UploadFileContainer>
-                                <GeneralButton buttonLabel="Upload file" />
+                                <FileInput onUpload={handleImageUpload} />
+                                <UploadFileText>
+                                    {uploadedFileName}
+                                </UploadFileText>
                             </UploadFileContainer>
-                            <DropContainer></DropContainer>
+                            <DropContainer>
+                                <DropZone
+                                    onDrop={handleImageDrop}
+                                    noClick
+                                    noKeyboard
+                                >
+                                    {({ getRootProps, getInputProps }) => (
+                                        <div {...getRootProps()}>
+                                            <AvatarEditor
+                                                image={avatarState.image}
+                                            />
+                                            <input {...getInputProps()} />
+                                        </div>
+                                    )}
+                                </DropZone>
+                            </DropContainer>
                         </PictureContainer>
                         <SettingsContainer></SettingsContainer>
                     </ContentWrapper>
