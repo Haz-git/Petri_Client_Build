08adbe4670ed0569e3e8167852ff94b13ea06328
Haz-git <zhouharry1@gmail.com>
2021-07-12 20:00:55 -0700

    Created burger menu for smaller devices. Added burger handlers as well.

--- frontend/src/components/Dashboard/settings/UserSettings.tsx
+++ frontend/src/components/Dashboard/settings/UserSettings.tsx
@@ -18,7 +18,25 @@ import Toggler from '../../../components/Styling/Toggler';
 
 //Styles:
 import defaultAvatar from '../../../Img/default_avatar.png';
+import { Menu } from '@styled-icons/entypo/Menu';
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline';
 import { deviceMin } from '../../../devices/breakpoints';
+
+const StyledClose = styled(CloseOutline)`
+    height: 2.5rem;
+    width: 2.5rem;
+    color: ${(props) => props.theme.text};
+    cursor: pointer;
+    margin: 0;
+`;
+
+const StyledMenu = styled(Menu)`
+    height: 2.5rem;
+    width: 2.5rem;
+    color: ${(props) => props.theme.text};
+    cursor: pointer;
+    margin: 0;
+`;
 
 const PageHeaderContainer = styled.div`
     padding: 1rem 1rem;
@@ -53,12 +71,11 @@ const LinkContainer = styled.div`
     flex-direction: column;
 
     @media ${deviceMin.laptopSs} {
-        flex-direction: row;
-        align-items: center;
-        justify-content: space-evenly;
+        display: none;
     }
 
     @media ${deviceMin.tablet} {
+        display: flex;
         flex-direction: column;
         align-items: stretch;
         justify-content: stretch;
@@ -89,6 +106,16 @@ const LinkButton = styled.button<LinkButtonProps>`
 
 const FormContainer = styled.div`
     padding: 2rem 4rem;
+    height: fit-content;
+    animation: all 0.5s ease-in-out;
+
+    @media ${deviceMin.laptopSs} {
+        padding: 1rem 2rem;
+    }
+
+    @media ${deviceMin.tablet} {
+        padding: 2rem 4rem;
+    }
 `;
 
 const ProfileImageContainer = styled.div`
@@ -96,10 +123,11 @@ const ProfileImageContainer = styled.div`
     border-bottom: 3px solid #f8f8ff;
 
     @media ${deviceMin.laptopSs} {
-        padding: 0;
+        display: none;
     }
 
     @media ${deviceMin.tablet} {
+        display: block;
         padding: 0;
     }
 `;
@@ -174,6 +202,59 @@ const AvatarSpacer = styled.div`
     margin: 1rem 0;
 `;
 
+//Styles for Burger menu on smaller screens:
+const BurgerContainer = styled.div`
+    display: block;
+    padding: 1rem 0 0 1.7rem;
+
+    @media ${deviceMin.laptopSs} {
+    }
+
+    @media ${deviceMin.tablet} {
+        display: none;
+    }
+`;
+
+const BurgerButton = styled.button`
+    margin: 0;
+    padding: 0;
+    background: none;
+    outline: none;
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+const BurgerLinkButton = styled.button`
+    font-family: 'Lato', sans-serif;
+    font-size: 1em;
+    font-weight: 700;
+    padding: 0.5rem 0.5rem;
+    text-align: left;
+    color: ${(props) => props.theme.text};
+    border: 1px solid #f8f8ff;
+    border-radius: 0.3rem;
+    background: rgba(0, 0, 34, 0.1);
+
+    outline: none;
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+const BurgerLinkContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 0 2rem;
+    max-width: 14.25rem;
+
+    @media ${deviceMin.tablet} {
+        display: none;
+    }
+`;
+
 interface LinkButtonProps {
     stateOnClick: boolean;
 }
@@ -213,6 +294,9 @@ const UserSettings = ({
     //Dark Mode Toggler:
     const [theme, toggleTheme] = useDarkMode();
 
+    //Burger Menu toggler:
+    const [burgerState, setBurgerState] = useState(false);
+
     //EditProfilePicture Modal states:
     const [profileModalState, setProfileModalState] = useState(false);
 
@@ -229,6 +313,11 @@ const UserSettings = ({
 
     //Settings view state handler:
     const [stateView, setStateView] = useState(RenderView.RENDER_USER_PROFILE);
+
+    //Burger Menu state handlers:
+    const openBurgerMenu = () => {
+        setBurgerState(!burgerState);
+    };
 
     //EditProfilePicture Modal state handlers:
     const openProfilePicModal = () => {
@@ -314,6 +403,39 @@ const UserSettings = ({
             </PageHeaderContainer>
             <MainContainer>
                 <ContentWrapper>
+                    <BurgerContainer>
+                        <BurgerButton onClick={openBurgerMenu}>
+                            {burgerState === false ? (
+                                <StyledMenu />
+                            ) : (
+                                <StyledClose />
+                            )}
+                        </BurgerButton>
+                    </BurgerContainer>
+                    {burgerState && (
+                        <BurgerLinkContainer>
+                            <BurgerLinkButton
+                                onClick={() =>
+                                    changeRenderView(
+                                        RenderView.RENDER_USER_PROFILE
+                                    )
+                                }
+                            >
+                                Personal Details
+                            </BurgerLinkButton>
+                            <BurgerLinkButton
+                                onClick={() =>
+                                    changeRenderView(RenderView.RENDER_PASSWORD)
+                                }
+                            >
+                                Password
+                            </BurgerLinkButton>
+                            <BurgerLinkButton>
+                                Security & Privacy
+                            </BurgerLinkButton>
+                            <BurgerLinkButton>Upgrade</BurgerLinkButton>
+                        </BurgerLinkContainer>
+                    )}
                     <LinkContainer>
                         <ProfileImageContainer>
                             {/* <Link
