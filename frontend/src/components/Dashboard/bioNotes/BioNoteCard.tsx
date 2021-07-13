5fc19764f75a76beb466d8c9120de2d45ecea19f
Haz-git <zhouharry1@gmail.com>
2021-07-13 09:43:10 -0700

    Converted files to tsx

--- frontend/src/components/Dashboard/bioNotes/BioNoteCard.tsx
+++ frontend/src/components/Dashboard/bioNotes/BioNoteCard.tsx
@@ -4,71 +4,64 @@ import { Link } from 'react-router-dom';
 import DeleteModalBioNote from './DeleteModalBioNote';
 
 //Styles:
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
 
 const MainCardContainer = styled.div`
     margin-top: 10px;
     margin-bottom: 10px;
-    background-color: ${props => props.theme.bionoteCardColor};
+    background-color: ${(props) => props.theme.bionoteCardColor};
     padding: 10px 10px;
-    border: ${props => props.theme.bionoteBorderColor};
+    border: ${(props) => props.theme.bionoteBorderColor};
     border-radius: 5px;
-    box-shadow:
-        0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-        0 6.7px 5.3px rgba(0, 0, 0, 0.048),
-        0 12.5px 10px rgba(0, 0, 0, 0.06),
+    box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+        0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
         0 22.3px 17.9px rgba(0, 0, 0, 0.072),
-        0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-        0 60px 40px rgba(0, 0, 0, 0.12);
-    
-`
+        0 41.8px 33.4px rgba(0, 0, 0, 0.086), 0 60px 40px rgba(0, 0, 0, 0.12);
+`;
 
 const CardInfoContainer = styled.div`
     display: block;
     text-align: center;
-`
+`;
 const LinkContainer = styled.div`
     display: flex;
     justify-content: center;
-`
+`;
 
 const StyledLink = styled(Link)`
     text-decoration: none;
     margin-left: 6px;
     margin-right: 6px;
-`
+`;
 const StyledButtonSpacer = styled.div`
     margin-left: 6px;
     margin-right: 6px;
-`
+`;
 
 const StyledTitle = styled.h2`
     font-size: 30px;
     font-family: 'Nunito', sans-serif;
     font-weight: 300;
-    color: ${props => props.theme.bionoteTextColor};
+    color: ${(props) => props.theme.bionoteTextColor};
     margin-bottom: 20px;
     overflow-wrap: break-word;
-
-`
+`;
 
 const BioNoteCard = ({ name, bionote_ID }) => {
-
     //renderDeleteModal controls modal provided by React-bootstrap.
 
-    const [ renderDeleteModal, setRenderDeleteModal ] = useState(false);
+    const [renderDeleteModal, setRenderDeleteModal] = useState(false);
 
-    const renderDeleteCallBack = boolean => {
+    const renderDeleteCallBack = (boolean) => {
         setRenderDeleteModal(boolean);
-    }
+    };
 
     const handleDeleteRequest = (e) => {
         e.preventDefault();
 
         setRenderDeleteModal(true);
-    }
-
+    };
 
     return (
         <>
@@ -77,19 +70,18 @@ const BioNoteCard = ({ name, bionote_ID }) => {
                     <StyledTitle>{name}</StyledTitle>
                     <hr />
                     <LinkContainer>
-                        <ButtonGroup size='lg' className='mb-2'>
+                        <ButtonGroup size="lg" className="mb-2">
                             <StyledLink to={`/readbionote/${bionote_ID}`}>
-                                <Button variant='success'>
-                                    Read
-                                </Button>
+                                <Button variant="success">Read</Button>
                             </StyledLink>
                             <StyledLink to={`/editbionote/${bionote_ID}`}>
-                                <Button variant='warning'>
-                                    Edit
-                                </Button>
+                                <Button variant="warning">Edit</Button>
                             </StyledLink>
                             <StyledButtonSpacer>
-                                <Button variant='danger' onClick={handleDeleteRequest}>
+                                <Button
+                                    variant="danger"
+                                    onClick={handleDeleteRequest}
+                                >
                                     Delete
                                 </Button>
                             </StyledButtonSpacer>
@@ -104,7 +96,7 @@ const BioNoteCard = ({ name, bionote_ID }) => {
                 bionote_ID={bionote_ID}
             />
         </>
-    )
-}
+    );
+};
 
 export default BioNoteCard;
--- frontend/src/components/Dashboard/bioNotes/CreateBioNote.tsx
+++ frontend/src/components/Dashboard/bioNotes/CreateBioNote.tsx
@@ -14,53 +14,47 @@ import { PencilSquare } from '@styled-icons/bootstrap/PencilSquare';
 import Fade from 'react-reveal/Fade';
 
 const MainContainer = styled.div`
-    background-color: ${props => props.theme.bionotesContainerBGColor};
+    background-color: ${(props) => props.theme.bionotesContainerBGColor};
     height: 100vh;
-`
-
+`;
 
 const MainBioNotesHeaderContainer = styled.div`
     padding-left: 40px;
     display: flex;
     text-align: center;
-    background-color: ${props => props.theme.settingsHeaderBG};
+    background-color: ${(props) => props.theme.settingsHeaderBG};
     height: 85px;
-    border-left: 1px solid #F6F9FC;
+    border-left: 1px solid #f6f9fc;
     justify-content: flex-start;
     align-items: center;
     margin: 0;
     padding-top: 0;
     padding-bottom: 0;
-    box-shadow:
-        0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-        0 1px 1px rgba(0, 0, 0, 0.048),
-        0 2px 2px rgba(0, 0, 0, 0.06),
-        0 3px 3px rgba(0, 0, 0, 0.072),
-        0 3px 4px rgba(0, 0, 0, 0.086),
-        0 2px 1px rgba(0, 0, 0, 0.12);
-    ;
-`
+    box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+        0 1px 1px rgba(0, 0, 0, 0.048), 0 2px 2px rgba(0, 0, 0, 0.06),
+        0 3px 3px rgba(0, 0, 0, 0.072), 0 3px 4px rgba(0, 0, 0, 0.086),
+        0 2px 1px rgba(0, 0, 0, 0.12); ;
+`;
 
 const StyledMainHeader = styled.h1`
     font-family: 'Montserrat', sans-serif;
     font-size: 50px;
     font-weight: 100;
-    color: ${props => props.theme.settingsMainHeaderTextC};
-`
+    color: ${(props) => props.theme.settingsMainHeaderTextC};
+`;
 
 const ButtonContainer = styled.div`
     text-align: center;
     margin-top: 5px;
     margin-bottom: 20px;
-`
+`;
 
 const PencilIcon = styled(PencilSquare)`
     height: 20px;
     width: 20px;
     vertical-align: sub;
     margin-left: 7px;
-`
-
+`;
 
 const NoteCardContainer = styled.div`
     display: grid;
@@ -68,31 +62,30 @@ const NoteCardContainer = styled.div`
     margin: 0 auto;
     max-width: 400px;
     grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-
-`
+`;
 
 //Render:
 
 const CreateBioNote = ({ bionotes, getBioNotes }) => {
-
     useEffect(() => {
-        getBioNotes()
-
-    },[]);
+        getBioNotes();
+    }, []);
 
     const renderNotes = () => {
         if (bionotes !== undefined && bionotes !== null) {
-            return (
-                bionotes.map(note => (
-                    <Fade>
-                        <BioNoteCard key={uuid()} name={note.bioName} bionote_ID={note.bionote_ID} />
-                    </Fade>
-                ))
-            )
+            return bionotes.map((note) => (
+                <Fade>
+                    <BioNoteCard
+                        key={uuid()}
+                        name={note.bioName}
+                        bionote_ID={note.bionote_ID}
+                    />
+                </Fade>
+            ));
         } else {
             return null;
         }
-    }
+    };
 
     return (
         <>
@@ -103,26 +96,23 @@ const CreateBioNote = ({ bionotes, getBioNotes }) => {
                     </Fade>
                 </MainBioNotesHeaderContainer>
                 <ButtonContainer>
-                        <Link to='newbionote'>
-                            <Button variant='primary'>
-                                Create a New BioNote
-                                <PencilIcon />
-                            </Button>
-                        </Link>
+                    <Link to="newbionote">
+                        <Button variant="primary">
+                            Create a New BioNote
+                            <PencilIcon />
+                        </Button>
+                    </Link>
                 </ButtonContainer>
-                <NoteCardContainer>
-                    {renderNotes()}
-                </NoteCardContainer>
+                <NoteCardContainer>{renderNotes()}</NoteCardContainer>
             </MainContainer>
         </>
-    )
-}
+    );
+};
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
         bionotes: state.bionotes.bionotes,
-    }
-}
-
+    };
+};
 
 export default connect(mapStateToProps, { getBioNotes })(CreateBioNote);
