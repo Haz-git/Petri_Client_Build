00f2ac60e2392d0060cc4fe3861772b53e56d13d
Haz-git <zhouharry1@gmail.com>
2021-07-14 10:44:56 -0700

    Implemented JWT check per route to ensure correct navbar format-- this will be deprecated as the app will soon be separated from website

--- frontend/src/components/landingPage/Navbar.tsx
+++ frontend/src/components/landingPage/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getJWT } from '../../utils/jwthelper';
 import { logoutHelper } from '../../utils/logouthelper';
@@ -364,10 +364,12 @@ const Navbar = ({ StateJwt }) => {
 
     const [JWT, setJWT] = useState(undefined);
 
+    const location = useLocation();
+
     useEffect(() => {
         const jwt2 = getJWT();
         setJWT(jwt2);
-    }, [StateJwt]);
+    }, [location]);
 
     const renderNavOnJWT = (jwt) => {
         if (
