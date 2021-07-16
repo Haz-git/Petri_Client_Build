05ed8dfe1a14dfc769d51d62577a5c968c835ef3
Haz-git <zhouharry1@gmail.com>
2021-07-15 19:09:01 -0700

    .

--- frontend/src/components/Dashboard/bioNotes/EditBioNote.js
+++ frontend/src/components/Dashboard/bioNotes/EditBioNote.js
@@ -3,12 +3,10 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { updateBioNote } from '../../../redux/userBioNote/bionoteActions';
 
-
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import Editor from 'ckeditor5-custom-build/build/ckeditor';
 import { editorConfiguration } from '../../../utils/ckeditortoolbar';
-import { unescape} from 'html-escaper';
-
+import { unescape } from 'html-escaper';
 
 //Styles:
 import styled from 'styled-components';
@@ -19,7 +17,7 @@ import { Save } from '@styled-icons/entypo/Save';
 
 const MainEditorContainer = styled.div`
     padding: 30px 30px;
-`
+`;
 
 const NameContainer = styled.div`
     text-align: center;
@@ -28,108 +26,102 @@ const NameContainer = styled.div`
     padding: 10px 10px;
     font-size: 25px;
     font-family: 'Montserrat', sans-serif;
-`
+`;
 
 const EditorContainer = styled.div`
     padding: 10px 10px;
     border: 1px solid #242746;
     background-color: white;
     overflow-y: auto;
-    box-shadow:
-        0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-        0 6.7px 5.3px rgba(0, 0, 0, 0.048),
-        0 12.5px 5px rgba(0, 0, 0, 0.06),
-        0 22.3px 10px rgba(0, 0, 0, 0.072),
-        0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-        0 60px 80px rgba(0, 0, 0, 0.12);
-    ;
-`
+    box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+        0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 5px rgba(0, 0, 0, 0.06),
+        0 22.3px 10px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+        0 60px 80px rgba(0, 0, 0, 0.12); ;
+`;
 
 const ButtonContainer = styled.div`
     display: flex;
     justify-content: center;
     padding: 20px 10px;
-`
+`;
 
 const StyledButton = styled(Button)`
     margin-left: 7px;
     margin-right: 7px;
-`
+`;
 
 const SaveIcon = styled(Save)`
     height: 23px;
     width: 23px;
     margin-right: 7px;
     vertical-align: sub;
-`
+`;
 const CaretIcon = styled(CaretBack)`
     height: 23px;
     width: 23px;
     margin-right: 7px;
     vertical-align: sub;
-`
-
-
+`;
 
 //Render:
 
-
-const EditBioNote = ({ match:{params:{id}}, bionotes, updateBioNote }) => {
-
-    const [ editorState, setEditorState ] = useState(null);
-    const [ bioNoteName, setBioNoteName ] = useState('');
-    const [ editorChange, setEditorChange ] = useState('');
+const EditBioNote = ({
+    match: {
+        params: { id },
+    },
+    bionotes,
+    updateBioNote,
+}) => {
+    const [editorState, setEditorState] = useState(null);
+    const [bioNoteName, setBioNoteName] = useState('');
+    const [editorChange, setEditorChange] = useState('');
 
     useEffect(() => {
-
         //Grab the bionote name from state.
-        const currentName = bionotes.bionotes.find(x => x.bionote_ID === id);
+        const currentName = bionotes.bionotes.find((x) => x.bionote_ID === id);
         setBioNoteName(currentName.bioName);
         findEditorState();
-    },[]);
-
+    }, []);
 
     const findEditorState = () => {
-        const currentNote = bionotes.bionotes.find(x => x.bionote_ID === id);
+        const currentNote = bionotes.bionotes.find((x) => x.bionote_ID === id);
         const fixedHTML = unescape(currentNote.htmlState);
         setEditorState(fixedHTML);
-    }
+    };
 
     const renderCKEditor = () => {
-        if (editorState === null ) {
+        if (editorState === null) {
             return null;
         } else {
             return (
                 <CKEditor
-                    editor={ Editor }
-                    config={ editorConfiguration }
-                    onChange={ handleCKEditorChange }
-                    data={ editorState }
+                    editor={Editor}
+                    config={editorConfiguration}
+                    onChange={handleCKEditorChange}
+                    data={editorState}
                 />
-            )
+            );
         }
-    }
-
+    };
 
     const handleCKEditorChange = (event, editor) => {
         const dataHTML = editor.getData();
 
-        setEditorChange(dataHTML)
-
-    }
+        setEditorChange(dataHTML);
+    };
 
     const onEditorSubmit = (e) => {
         e.preventDefault();
         updateBioNote(id, editorChange);
-    }
+    };
 
     const renderName = () => {
         return (
             <Fade>
                 <NameContainer>{bioNoteName}</NameContainer>
             </Fade>
-        )
-    }
+        );
+    };
 
     return (
         <>
@@ -137,17 +129,19 @@ const EditBioNote = ({ match:{params:{id}}, bionotes, updateBioNote }) => {
                 <MainEditorContainer>
                     {renderName()}
                     <form onSubmit={onEditorSubmit}>
-                        <EditorContainer>
-                            {renderCKEditor()}
-                        </EditorContainer>
+                        <EditorContainer>{renderCKEditor()}</EditorContainer>
                         <ButtonContainer>
-                            <Link to='/createbionote'>
-                                <StyledButton variant='secondary' size='lg'>
+                            <Link to="/createbionote">
+                                <StyledButton variant="secondary" size="lg">
                                     <CaretIcon />
                                     Your Existing Bionotes
                                 </StyledButton>
                             </Link>
-                            <StyledButton variant='success' type='submit' size='lg'>
+                            <StyledButton
+                                variant="success"
+                                type="submit"
+                                size="lg"
+                            >
                                 <SaveIcon />
                                 Save
                             </StyledButton>
@@ -156,14 +150,13 @@ const EditBioNote = ({ match:{params:{id}}, bionotes, updateBioNote }) => {
                 </MainEditorContainer>
             </Fade>
         </>
-    )
-}
+    );
+};
 
-const mapStateToProps = state => {
-
+const mapStateToProps = (state) => {
     return {
         bionotes: state.bionotes,
-    }
-}
+    };
+};
 
 export default connect(mapStateToProps, { updateBioNote })(EditBioNote);
