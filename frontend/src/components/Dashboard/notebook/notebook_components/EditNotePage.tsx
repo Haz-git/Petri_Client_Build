f9b25d8a15a41f16a1e12c21d038925cfa2fc055
Haz-git <zhouharry1@gmail.com>
2021-07-26 05:41:40 -0700

    Created page for editing and reading notes

---
+++ frontend/src/components/Dashboard/notebook/notebook_components/EditNotePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const EditNotePage = () => {
+    return <div>This is a page for editing notes</div>;
+};
+
+export default EditNotePage;
