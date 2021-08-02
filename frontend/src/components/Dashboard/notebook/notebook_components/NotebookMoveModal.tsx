9016f3c06ce77e43045623ab302a42fb4d4a48da
Haz-git <zhouharry1@gmail.com>
2021-08-02 07:12:47 -0700

    Enabled rendering of search bar

--- frontend/src/components/Dashboard/notebook/notebook_components/NotebookMoveModal.tsx
+++ frontend/src/components/Dashboard/notebook/notebook_components/NotebookMoveModal.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 //Components:
 import Modal from '@material-ui/core/Modal';
 import GeneralButton from '../../general_components/GeneralButton';
+
+//Redux:
+import { connect } from 'react-redux';
+import { getNotebook } from '../../../../redux/userNotebook/notebookActions';
 
 //Styles:
 import {
@@ -15,8 +19,18 @@ import {
     ButtonSpacer,
     Divider,
 } from '../notebook_components/NotebookRenameModal';
+import NotebookSearchDropdown from '../notebook_components/NotebookSearchDropdown';
 
 //Interfaces:
+
+interface IDispatchProps {
+    getNotebook: (statusCallback: (status: boolean) => void) => void;
+}
+
+interface IMapStateToProps {
+    notebook: { rootFiles: any; rootFolders: any };
+}
+
 interface IComponentProps {
     openState: boolean;
     closeFunc: () => void;
@@ -26,6 +40,10 @@ interface IComponentProps {
     entityId?: string;
 }
 
+type NotebookMoveModalProps = IDispatchProps &
+    IMapStateToProps &
+    IComponentProps;
+
 const NotebookMoveModal = ({
     openState,
     closeFunc,
@@ -33,7 +51,16 @@ const NotebookMoveModal = ({
     entityId,
     entityName,
     entityParentId,
-}: IComponentProps): JSX.Element => {
+    getNotebook,
+    notebook,
+}: NotebookMoveModalProps): JSX.Element => {
+    const [isNotebookLoaded, setIsNotebookLoaded] = useState(false);
+
+    //Loaded State handler
+    const setLoadedStatus = (status: boolean) => setIsNotebookLoaded(status);
+
+    console.log(notebook);
+
     return (
         <Modal open={openState} onClose={closeFunc}>
             <ModalContainer>
@@ -41,9 +68,16 @@ const NotebookMoveModal = ({
                 <ModalDescText>
                     Please select the new location of your item.
                 </ModalDescText>
+                <NotebookSearchDropdown notebookEntities={notebook} />
             </ModalContainer>
         </Modal>
     );
 };
 
-export default NotebookMoveModal;
+const mapStateToProps = (state: any, ownProps: any) => {
+    return {
+        notebook: state.notebook.notebook,
+    };
+};
+
+export default connect(mapStateToProps, { getNotebook })(NotebookMoveModal);
